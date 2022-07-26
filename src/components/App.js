97f8d91b8dcefd1824c1ddbff744e9c94b9eeb9b
Keith <keithwesley187@gmail.com>
2022-07-26 23:47:55 +0300

    Default to Darkmode

--- src/components/App.js
+++ src/components/App.js
@@ -3,39 +3,21 @@ import FeedBack from "./FeedBack";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CryptoTracker from './CryptoTracker';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NavBar from './NavBar';
-import styled, { ThemeProvider } from "styled-components"
-import { lightTheme, darkTheme, GlobalStyle } from "./Themes"
 import HomePage from './HomePage';
-
-const StyledApp =  styled.div`
-color: ${props => props.theme.color}
-`;
 
 function App() {
 
-  const [theme, setTheme] = useState("dark");
-
-  const changeTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light")
-  }
-
   return (
-    <ThemeProvider theme={theme ===  "light" ? lightTheme : darkTheme }>
-    <GlobalStyle />
-    <StyledApp>
-      <button onClick={() => changeTheme()}ToggleDarkLight>Theme</button>
-      <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact="true" path="/" element={<HomePage />}/>
-        <Route exact="true" path="/cryptotracker" element={<CryptoTracker />}/>
-        <Route path='/feedback' element={<FeedBack />}/>
-      </Routes>
-      </BrowserRouter>
-    </StyledApp>
-    </ThemeProvider>
+  <BrowserRouter>
+  <NavBar />
+  <Routes>
+    <Route exact="true" path="/" element={<HomePage />}/>
+    <Route exact="true" path="/cryptotracker" element={<CryptoTracker />}/>
+    <Route path='/feedback' element={<FeedBack />}/>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
